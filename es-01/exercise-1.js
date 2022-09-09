22918d6339f9af61d5edeacd06f8712192afcc89
Ignazioconti <108429510+Ignazioconti@users.noreply.github.com>
2022-09-09 15:18:59 +0200

    esercizio-1 dichiarazione delle variabili

---
+++ es-01/exercise-1.js
@@ -0,0 +1,11 @@
+let personName;
+function canPlay() {
+  if (true) {
+    personName = "Paul";
+  }
+
+  personName += " plays football"; // Argh! personName is not defined
+
+  console.log(personName);
+}
+canPlay();
